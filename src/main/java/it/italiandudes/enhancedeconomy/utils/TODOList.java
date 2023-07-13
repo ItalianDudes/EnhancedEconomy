9799aa610f45004535e8f30cafde229517d8537e
Hacker6329 <Hacker6329@gmail.com>
2023-07-13 18:36:50 +0200

    Added TODOList

---
+++ src/main/java/it/italiandudes/enhancedeconomy/utils/TODOList.java
@@ -0,0 +1,7 @@
+package it.italiandudes.enhancedeconomy.utils;
+
+public class TODOList {
+    //TODO: Testare EEBankCurrency
+    //TODO: Linkare alle i conti correnti alle banche, tenendo conto del denaro della banca
+    //TODO: Fare i conti correnti
+}
